c442a298ff04f707574160adf17949320a4e1101
Anu Raife <anuradharaife2020@u.northwestern.edu>
2020-05-29 17:24:03 -0500

    updated steps 1-3

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+gameboard = [['-','-','-'],['-','-','-'],['-','-','-']]
+
 def tic_tac_toe():
     print("welcome to tic tac toe")
-    user = input("type play to play or quit to quit")
+    user = input("type 'play' to play or 'quit' to quit")
     if user == "play":
         print("decide who's going first")
-        game = ['-','-','-']['-','-','-']['-','-','-']
-        print "here are the cordinates to enter for each position on ur game board for refrence: /n[.(1,1)   (1,2)   (1,3)] \n[(2,1)   (2,2)   (2,3)] \n [(3,1)   (3,2)   (3,3)]”*
-        for row in game:
-            print(row)
-            guess = input("put in a coordinate with no spaces, parnethesis, or commas")
-            for num in guess:
-                num = num - 1
-                if **gameboard[guess[0][guess[1]** = '-':
-                    **gameboard[guess[0][guess[1]** = 'X"
-                    player_2 = input("ok player 2 now yout turn")
-                    if **gameboard[guess[0][guess[1]** = '-':
-                    **gameboard[guess[0][guess[1]** = 'O'
-                else:
-                    print "this space is taken please try another space"
+        print("here are the cordinates to enter for each position on ur game board for reference:")
+        print("[(1,1)   (1,2)   (1,3)] \n")
+        print("[(2,1)   (2,2)   (2,3)] \n")
+        print("[(3,1)   (3,2)   (3,3)] \n")
+        print(" ~~~~~~~~~~STARTING GAME~~~~~~~~~~")
+        move_player('X')
+        move_player('O')
+
+def move_player(player):
+    print("this is the current gameboard")
+    for row in gameboard:
+        print(row)
+    guess = list(input("Player %s put in the coordinates of your move. Please don’t use parenthesis, commas or spaces though. For example, if you want to take (1,2) just enter 12" %player))
+    for index, num in enumerate(guess):
+        guess[index] = int(num) - 1
+    if gameboard[guess[0]][guess[1]] == '-':
+        gameboard[guess[0]][guess[1]] = player
+    else:
+        print("this space is taken please try another space")
+        move_player(player)
